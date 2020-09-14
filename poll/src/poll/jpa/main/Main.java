46ff466adff0cae7cd682823d870da1de4eca7a6
crissb3 <69694126+crissb3@users.noreply.github.com>
2020-09-14 19:06:04 +0200

    Update Main.java

--- poll/src/poll/jpa/main/Main.java
+++ poll/src/poll/jpa/main/Main.java
@@ -1,5 +1,28 @@
 package poll.jpa.main;
 
-public class Main {
+private static final String PERSISTENCE_UNIT_NAME = "poll";
+    private static EntityManagerFactory factory;
 
-}
+    public static void main(String[] args) {
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        User u = new User();
+        u.setUsername("er11");
+        
+   
+        em.persist(u);
+         
+        em.getTransaction().commit();
+
+        Query q = em.createQuery("select u from User u");
+        List<User> uList = q.getResultList();
+        for (User user : uList) {
+            System.out.println(u.getUsername());
+          
+        }
+        System.out.println("Size: " + uList.size());
+
+        em.close();
