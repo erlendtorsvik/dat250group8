abc81827e39bc885d38c6a3059f5014c2184a185
crissb3 <69694126+crissb3@users.noreply.github.com>
2020-09-14 22:32:11 +0200

    Update VoteDAO.java

--- poll/src/poll/jpa/main/VoteDAO.java
+++ poll/src/poll/jpa/main/VoteDAO.java
@@ -1,24 +1,58 @@
 package poll.jpa.main;
-import javax.persistence.*;
 
-public class VoteDAO {
-	
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import model.Vote;
+
+public class VoteDAO implements Dao<Vote>{
 	@PersistenceContext
 	EntityManager em;
-	
-	void create(Vote vote) {
-		em.persist(vote);
+	public VoteDAO()
+	{
+		final String PERSISTENCE_UNIT_NAME = "poll";
+		EntityManagerFactory factory;
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        em = factory.createEntityManager();
 	}
-	
-	Optional<Vote> find(int id) {
+	@Override
+	public void create(Vote vote) {
+		em.getTransaction().begin();
+		em.persist(vote);
+		em.getTransaction().commit();
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Vote> find()
+	{
+		Query query = em.createQuery("SELECT v FROM Vote v");
+        return query.getResultList();
+	}
+	@Override
+	public Vote find(int id) {
 		return em.find(Vote.class, id);
 	}
-	
-	void save(Vote vote) {
-		em.save(vote);
+	@Override
+	public void save(Vote vote) {
+		((VoteDAO) em).save(vote);
 	}
-	
-	//void update(Vote vote) {
-	//	em.merge(user);
-	//}
+	@Override
+	public void update(Vote vote) {
+		em.getTransaction().begin();
+		em.merge(vote);
+		em.getTransaction().commit();
+	}
+	@Override
+	public void delete(int id) {
+		Vote v = em.find(Vote.class, id);
+		em.getTransaction().begin();
+		em.remove(v);
+		em.getTransaction().commit();
+	}
 }
