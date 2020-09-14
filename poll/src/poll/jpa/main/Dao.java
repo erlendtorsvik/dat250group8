0ed7822e919135ea25e531ae4c3f45a9b779c593
crissb3 <69694126+crissb3@users.noreply.github.com>
2020-09-14 22:33:06 +0200

    Create Dao.java

---
+++ poll/src/poll/jpa/main/Dao.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.List;
+
+
+public interface Dao<T> {
+    
+    
+	List<T> find();
+    
+    T find(int id);
+    
+    void create(T t);
+    
+    void save(T t);
+    
+    void update(T t);
+    
+    void delete(int id);
+    
+    
+    
+}
