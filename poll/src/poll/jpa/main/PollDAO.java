87af03999a454f95c99e62e4f51e6d76533fc560
crissb3 <69694126+crissb3@users.noreply.github.com>
2020-09-14 22:30:04 +0200

    Update PollDAO.java

--- poll/src/poll/jpa/main/PollDAO.java
+++ poll/src/poll/jpa/main/PollDAO.java
@@ -1,24 +1,64 @@
 package poll.jpa.main;
-import javax.persistence.*;
 
-public class PollDAO {
-	
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import model.Poll;
+
+
+public class PollDAO implements Dao<Poll>{
 	@PersistenceContext
 	EntityManager em;
-	
-	void create(Poll poll) {
-		em.persist(poll);
+	public PollDAO()
+	{
+		final String PERSISTENCE_UNIT_NAME = "poll";
+		EntityManagerFactory factory;
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        em = factory.createEntityManager();
 	}
 	
-	Optional<Poll> find(int id) {
+	@Override
+	public void create(Poll poll) {
+		em.getTransaction().begin();
+		em.persist(poll);
+		em.getTransaction().commit();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Poll> find(){
+		Query query = em.createQuery("SELECT p FROM Poll p");
+        return query.getResultList();
+	}
+	
+	@Override
+	public Poll find(int id) {
 		return em.find(Poll.class, id);
 	}
 	
-	void save(Poll poll) {
-		em.save(poll);
+	@Override
+	public void save(Poll poll) {
+		((PollDAO) em).save(poll);
 	}
 	
-	void update(Poll poll) {
+	@Override
+	public void update(Poll poll) {
+		em.getTransaction().begin();
 		em.merge(poll);
+		em.getTransaction().commit();
+	}
+	@Override
+	public void delete(int id)
+	{
+		Poll p = em.find(Poll.class, id);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 	}
 }
